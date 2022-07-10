406ae56244630c31e6a8d84d6f1f77d0a6684781
kamilajesus <kamilaalves727@gmail.com>
2022-07-10 19:19:59 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Cardapio from "./pages/Cardapio";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cardapio />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Cardapio.module.scss";
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <img src="../../assets/logo.svg" alt="logo do aluroni" />
+      </nav>
+    </main>
+  )
+}
