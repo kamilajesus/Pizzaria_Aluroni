132e72f2bf6b43565a1c2b625516f51319d3fcb3
kamilajesus <kamilaalves727@gmail.com>
2022-07-14 22:04:39 -0300

    new commit

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import styles from './Buscador.module.scss';
-import { CgSearch } from 'react-icons/cg';
+import styles from "./Buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
 
-interface Props{
+interface Props {
   busca: string;
   setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({busca, setBusca}: Props){
-  return(
+export default function Buscador({ busca, setBusca }: Props) {
+  return (
     <div className={styles.buscador}>
-      <input 
-      value={busca} 
-      onChange={evento => setBusca(evento.target.value)}
-      placeholder="Buscar"
+      <input
+        value={busca}
+        onChange={(evento) => setBusca(evento.target.value)}
+        placeholder="Buscar"
       />
-      <CgSearch 
-      size={20}
-      color="#4C4D5E"
-      />
+      <CgSearch size={20} color="#4C4D5E" />
     </div>
-  )
+  );
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,10 +3,13 @@ import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
 import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from './Itens';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
   const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
   return (
     <main>
       <nav className={styles.menu}>
@@ -22,7 +25,9 @@ export default function Cardapio() {
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   )
